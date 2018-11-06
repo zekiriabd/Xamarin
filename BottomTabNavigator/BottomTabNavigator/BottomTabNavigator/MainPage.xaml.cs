5369b9d4d4a7b1277069ca6d653dd52796d0814e
Zekiriabd <Zekiriabd@yahoo.fr>
2018-11-06 09:11:33 +0100

    Bottom Tab Navigator

---
+++ BottomTabNavigator/BottomTabNavigator/BottomTabNavigator/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace BottomTabNavigator
+{
+    public partial class MainPage : Xamarin.Forms.TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
+            On<Android>().SetBarSelectedItemColor(Color.White);
+            On<Android>().SetBarItemColor(Color.White);
+
+            //On<iOS>().SetBarTintColor(Color.Red);                 // Unselected image+text color
+            //On<iOS>().SetUnselectedItemTintColor(Color.White); // Selected image+text color
+        }
+
+        private void MyPage_CurrentPageChanged(object sender, System.EventArgs e)
+        {
+            MyPage.BarBackgroundColor = ((ContentPage)MyPage.CurrentPage).BackgroundColor;
+        }
+    }
+}
