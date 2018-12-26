f67a75283ede1134baf0a7caee5faf10af2846e1
zekiriabd <zekiriabd@gmail.com>
2018-12-26 15:17:31 +0100

    BarCodeScanner

---
+++ BarCodeScanner/BarCodeScanner/BarCodeScanner/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarCodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var scanBarObject = new ZXingScannerPage();
+            await Navigation.PushAsync(scanBarObject);
+            scanBarObject.OnScanResult += (result) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    CodeBarText.Text = result.Text;
+                });
+            };
+        }
+    }
+}
